365dd9431a64c6dfceb7d67eaa08c79ae8d44f4a
David Lin <david@Davids-MacBook-Air.local>
2018-03-28 10:03:34 +0800

    including css

--- teethtest.js
+++ teethtest.js
@@ -1,1 +1,6 @@
-teethtest.textContent = 'hello';
+var a = ['右上','左上','左下','右下']
+var b = ['正門齒','側門齒','第一乳臼齒','第二乳臼齒','犬齒','第一小臼齒','第二小臼齒','第一大臼齒','第二大臼齒']
+var c = ['近心','遠心','舌側','頰側','咬合']
+teethtest.textContent = a[Math.floor(Math.random()*4)] +
+                        b[Math.floor(Math.random()*9)] +
+                        c[Math.floor(Math.random()*5)];
