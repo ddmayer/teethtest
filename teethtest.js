df3dcf25b4d7fb348123fe07af3b646371fe4779
David Lin <shingjijoo@gmail.com>
2018-03-29 16:17:56 +0800

    data updated

--- teethtest.js
+++ teethtest.js
@@ -1,6 +1,15 @@
-var a = ['右上','左上','左下','右下']
-var b = ['正門齒','側門齒','第一乳臼齒','第二乳臼齒','犬齒','第一小臼齒','第二小臼齒','第一大臼齒','第二大臼齒']
-var c = ['近心','遠心','舌側','頰側','咬合']
-teethtest.textContent = a[Math.floor(Math.random()*4)] +
-                        b[Math.floor(Math.random()*9)] +
-                        c[Math.floor(Math.random()*5)];
+var a = ['右上','左上','左下','右下'];
+var b = ['正中門齒','正乳門齒','側門齒','側乳門齒','犬齒','乳犬齒',
+        '第一乳臼齒','第二乳臼齒','第一小臼齒','第二小臼齒','第一大臼齒','第二大臼齒'];
+var c = ['近心','遠心','舌側','頰側','咬合'];
+var d = ['齲齒','填補且有蛀牙','填補'];
+
+var x = Math.floor(Math.random()*4);
+var y = Math.floor(Math.random()*12);
+if (y < 6) {
+    var z = Math.floor(Math.random() * 4);
+}else {
+    var z = Math.floor(Math.random() * 5);
+}
+var s = Math.floor(Math.random()*3);
+teethtest.textContent = a[x] + b[y] + c[z] + d[s];
